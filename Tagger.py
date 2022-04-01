52046c3ae2c0697e191a874b691a30b4c4ce0edb
Alisafarli <102352339+Alisafarli@users.noreply.github.com>
2022-04-02 01:23:48 +0400

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -24,35 +24,35 @@ tekli_calisan = []
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("**🇦🇿👨‍💻 CrstalTaggerbot**\n ile Grubunuzdakı Nerdeyse Tüm Üyelere Etiket Ata bilirim \nKomutlar için =======> /help yazın**",
+  await event.reply("**🇦🇿👨‍💻 KrakadilTaggerbot**\n ile Grubunuzdakı Nerdeyse Tüm Üyelere Etiket Ata bilirim \nKomutlar için =======> /help yazın**",
                     buttons=(
                    
-		      [Button.url('Beni Gruba Ekle ➕', 'https://t.me/CrstalTaggerbot?startgroup=a')],
-                      [Button.url('Support🛠', 'https://t.me/SU57support')],
-                      [Button.url('Resmi Kanal📣', 'https://t.me/SU57project')],
-		      [Button.url('SAHİBİM👨🏻‍💻', 'https://t.me/OwnerCrstal')],
+		      [Button.url('Beni Gruba Ekle ➕', 'https://t.me/KrakadilTaggerbot?startgroup=a')],
+                      [Button.url('Support🛠', 'https://t.me/Krakadilsupport')],
+                      [Button.url('Resmi Kanal📣', 'https://t.me/KMGResmi')],
+		      [Button.url('SAHİBİM👨🏻‍💻', 'https://t.me/AliSafarofh')],
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**🇦🇿👨‍💻 CrsralTaggerbot Komutları**\n\n**/tag <sebeb> - 5-li Etiket Atar**\n\n**/etag <sebeb> - Emoji ile etiketler**\n\n**/tektag sebeb - Üyeleri Tek Tek Etiketler**\n\n**/admins sebeb - Yöneticileri Tek Tek Tag Eder**\n\n**/start - botu başlatır**"
+  helptext = "**🇦🇿👨‍💻 KrakadilTaggerbot Komutları**\n\n**/tag <sebeb> - 5-li Etiket Atar**\n\n**/etag <sebeb> - Emoji ile etiketler**\n\n**/tektag sebeb - Üyeleri Tek Tek Etiketler**\n\n**/admins sebeb - Yöneticileri Tek Tek Tag Eder**\n\n**/start - botu başlatır**"
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('Beni Gruba Ekle➕', 'https://t.me/CrstalTaggerbot?startgroup=a')],
-                      [Button.url('Support👨‍💻', 'https://t.me/SU57support')],
-                      [Button.url('Resmi Kanal🔖', 'https://t.me/SU57project')],
-		      [Button.url('SAHİBİM🧑‍🔧', 'https://t.me/OwnerCrstal')],
+                      [Button.url('Beni Gruba Ekle➕', 'https://t.me/KrakadilTaggerbot?startgroup=a')],
+                      [Button.url('Support👨‍💻', 'https://t.me/Krakadilsupport')],
+                      [Button.url('Resmi Kanal🔖', 'https://t.me/KMGResmi')],
+		      [Button.url('SAHİBİM🧑‍🔧', 'https://t.me/AliSafarofh')],
                     ),
                     link_preview=False
                    )
 	
 @client.on(events.NewMessage(pattern="^/reklam$"))
 async def help(event):
-  helptext = "**Çok özellikleri Etiket Botu Bulmaya Çalışan Grub Sahibleri @CrstalTaggerbot Size Göre:\n\n📌 5-li etiket\n📌 Emoji etiket\n📌 Tekli Etiket\n📌 Yalnız Yöneticileri etiketleme\n📌\n\n Böyle Çok özellikli @LuciTaggerBot 'u grubunuza yönetici olarak ekleyip rahatlıkla üyelir , etiket ata bilirsiz **"
+  helptext = "**Çok özellikleri Etiket Botu Bulmaya Çalışan Grub Sahibleri @KrakadilTaggerbot Size Göre:\n\n📌 5-li etiket\n📌 Emoji etiket\n📌 Tekli Etiket\n📌 Yalnız Yöneticileri etiketleme\n📌\n\n Böyle Çok özellikli @LuciTaggerBot 'u grubunuza yönetici olarak ekleyip rahatlıkla üyelir , etiket ata bilirsiz **"
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('Botu Gruba Ekle➕', 'https://t.me/CrstalTaggerbot?startgroup=a')],
+                      [Button.url('Botu Gruba Ekle➕', 'https://t.me/KrakadilTaggerbot?startgroup=a')],
                     ),
                     link_preview=False
                    )
@@ -284,5 +284,5 @@ async def mentionall(tagadmin):
 		sleep(0.5)
 
 
-print(">> Bot çalışıyor merak etme 🚀 @CrstalOwner bilgi alabilirsin <<")
+print(">> Bot çalışıyor merak etme 🚀 @AliSafarofh bilgi alabilirsin <<")
 client.run_until_disconnected()
